87e50cdec567f575adfeb0259676f7121969d373
Blake Erickson <blake@discourse.org>
2025-04-22 14:00:03 -0600

    DEV: Add more system tests
    Added more system tests

--- spec/system/route_blocker_spec.rb
+++ spec/system/route_blocker_spec.rb
@@ -5,11 +5,31 @@ require "rails_helper"
 RSpec.describe "Route Blocker", type: :system do
   before do
     SiteSetting.route_blocker_enabled = true
-    SiteSetting.route_blocker_blocked_routes = "about"
+    SiteSetting.route_blocker_blocked_routes = "about|site/statistics|faq"
   end
 
   it "blocks access to the about page" do
     visit "/about"
     expect(page).to have_current_path("/404")
+  end
+
+  it "blocks access to the about.json endpoint" do
+    visit "/about.json"
+    expect(page).to have_content('{"error":"Not Found"}')
+  end
+
+  it "blocks access to the site statistics page" do
+    visit "/site/statistics"
+    expect(page).to have_css(".page-not-found")
+  end
+
+  it "blocks access to the site statistics.json endpoint" do
+    visit "/site/statistics.json"
+    expect(page).to have_content('{"error":"Not Found"}')
+  end
+
+  it "blocks access to the faq page" do
+    visit "/faq"
+    expect(page).to have_css(".still-loading")
   end
 end
