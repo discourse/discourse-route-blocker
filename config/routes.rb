1ba58960e8319e1d84d9c898f14490284c5a7ed2
Blake Erickson <blake@discourse.org>
2025-04-22 06:14:07 -0600

    rename more things to route-blocker

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-MyPluginModule::Engine.routes.draw do
+RouteBlocker::Engine.routes.draw do
   get "/examples" => "examples#index"
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "my-plugin" }
+Discourse::Application.routes.draw { mount ::RouteBlocker::Engine, at: "route-blocker" }
